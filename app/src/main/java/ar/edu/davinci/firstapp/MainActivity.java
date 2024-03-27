794c401bf2d1171d43bdf9217825f177e260b6a6
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-03-27 20:28:01 -0300

    feat: agregar clase Calculator

--- app/src/main/java/ar/edu/davinci/firstapp/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/firstapp/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import calculator.Calculator;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calculator calculator = new Calculator();
     }
 }
---
+++ app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    //TODO: feat: agregar metodo de suma de dos enteros
+
+    //TODO: feat: agregar metodo de multiplicacion de dos enteros
+
+    //TODO: feat: agregar metodos de resta de dos enteros
+
+    //TODO: feat: agregar metodo de division de dos enteros
+}
