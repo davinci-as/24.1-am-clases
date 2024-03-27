f5aa9c70b3d7ea0fc19b28e956c064730a55257f
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2024-03-27 20:48:22 -0300

    feat: funcion suma y multiplicacion (#1)
    Co-authored-by: Amalgaman <112218606+Amalgaman@users.noreply.github.com>

--- app/src/main/java/calculator/Calculator.java
+++ app/src/main/java/calculator/Calculator.java
@@ -2,9 +2,21 @@ package calculator;
 
 public class Calculator {
     //TODO: feat: agregar metodo de suma de dos enteros
+        public int sumar(int n1, int n2){
+            int resultado;
+            
+            resultado = n1 + n2;
 
+            return resultado;
+        }
     //TODO: feat: agregar metodo de multiplicacion de dos enteros
+        public int multiplicar(int n1, int n2){
+            int resultado;
+            
+            resultado = n1 * n2;
 
+            return resultado;
+        }
     //TODO: feat: agregar metodos de resta de dos enteros
 
     //TODO: feat: agregar metodo de division de dos enteros
