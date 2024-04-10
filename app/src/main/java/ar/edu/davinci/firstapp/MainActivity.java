90157369d6042c51b67d08e15a407838233171f2
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-04-10 20:42:15 -0300

    feat: agregar botones en la calculadora

--- app/src/main/java/ar/edu/davinci/firstapp/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/firstapp/MainActivity.java
@@ -3,10 +3,35 @@ package ar.edu.davinci.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import calculator.Calculator;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void sumar(View view) {
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.sumar);
+
+    }
+    public void restar(View view) {
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.restar);
+    }
+    public void multiplicar(View view) {
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.multiplicar);
+    }
+    public void dividir(View view) {
+        Log.i("app", "funciona");
+        TextView displayText = findViewById(R.id.displayText);
+        displayText.setText(R.string.dividir);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
